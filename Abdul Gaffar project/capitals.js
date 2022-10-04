916e8114ca157adf41aba775b1319b6d3f3b3bbe
AbdulGaffarAhmed <112863356+AbdulGaffarAhmed@users.noreply.github.com>
2022-10-04 14:49:49 +0530

    Add files via upload

---
+++ Abdul Gaffar project/capitals.js
@@ -0,0 +1,83 @@
+var c = 0;
+                function change(x){
+                    var x;
+                    if(x == 1)
+                    {
+                        c = c+1;
+                    }
+                    if(c == 1)
+                    {
+                        document.getElementById("a").style.background="red";
+                       document.getElementById("b").style.background="rgb(13, 245, 13)";
+                        document.getElementById("c").style.background="red";
+                        document.getElementById("d").style.background="red";
+
+                    }
+          
+                }
+                var b = 0;
+                function achange(y){
+                    var y;
+                    if(y == 1)
+                    {
+                        b = b+1;
+                    }
+                    if(b == 1)
+                    {
+                        document.getElementById("e").style.background="red";
+                       document.getElementById("f").style.background="red";
+                        document.getElementById("g").style.background="red";
+                        document.getElementById("h").style.background="rgb(13, 245, 13)";
+
+                    }
+             
+                }
+                var d = 0;
+                function bchange(z){
+                    var z;
+                    if(z == 1)
+                    {
+                        d = d+1;
+                    }
+                    if(d == 1)
+                    {
+                        document.getElementById("i").style.background="red";
+                       document.getElementById("j").style.background="red";
+                        document.getElementById("k").style.background="red";
+                        document.getElementById("l").style.background="rgb(13, 245, 13)";
+
+                    }
+                 
+                }
+                var e = 0;
+                function cchange(m){
+                    var m;
+                    if(m == 1)
+                    {
+                        e = e+1;
+                    }
+                    if(e == 1)
+                    {
+                        document.getElementById("m").style.background="rgb(13, 245, 13";
+                       document.getElementById("n").style.background="red";
+                        document.getElementById("o").style.background="red";
+                        document.getElementById("p").style.background="red";
+
+                    }
+                }
+                var f = 0;
+                function dchange(n){
+                    var n;
+                    if(n == 1)
+                    {
+                        f = f+1;
+                    }
+                    if(f == 1)
+                    {
+                        document.getElementById("q").style.background="red";
+                       document.getElementById("r").style.background="red";
+                        document.getElementById("s").style.background="rgb(13, 245, 13)";
+                        document.getElementById("t").style.background="red";
+
+                    }
+                }
